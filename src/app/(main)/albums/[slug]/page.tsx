24e5f81bda9606f03d3e730b7bdfe6972c350821
Max McKinney <max@maxmckinney.com>
2025-03-15 21:42:52 -0500

    fix: type error

--- src/app/(main)/albums/[slug]/page.tsx
+++ src/app/(main)/albums/[slug]/page.tsx
@@ -2,6 +2,8 @@ import SectionTitle from "@/components/SectionTitle";
 import GalleryImage from "@/components/GalleryImage";
 import { getAlbum } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { Key } from "react";
 
 export default async function Home({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
@@ -12,7 +14,7 @@ export default async function Home({ params }: { params: Promise<{ slug: string 
             <section className="@container">
                 <SectionTitle title={album?.title} description={album?.description} />
                 <div className="grid grid-cols-1 gap-4 @lg:grid-cols-2 @2xl:grid-cols-3 @3xl:grid-cols-5">
-                    {album.images?.map((photo, i) => (
+                    {album.images?.map((photo: SanityImageSource, i: Key | null | undefined) => (
                         <div className="aspect-square relative" key={i}>
                             <GalleryImage src={urlFor(photo).width(800).url() || ""} />
                         </div>
