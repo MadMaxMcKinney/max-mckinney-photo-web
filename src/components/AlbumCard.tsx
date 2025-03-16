e55f1a1e29349438230d638c021b2da811e75ac8
Max McKinney <max@maxmckinney.com>
2025-03-16 13:43:36 -0500

    component: add arrow anim to AlbumCard

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import React from "react";
 import { urlFor } from "@/sanity/lib/image";
+import { ArrowRight } from "lucide-react";
 
 interface AlbumCardProps extends React.ComponentProps<"a"> {
     album: any;
@@ -10,10 +11,17 @@ interface AlbumCardProps extends React.ComponentProps<"a"> {
 export default function AlbumCard({ album, ...props }: AlbumCardProps) {
     return (
         <Link href={`/albums/${album.slug.current}`} key={props.key}>
-            <Card className="pt-0 overflow-clip">
+            <Card className="group hover:bg-accent overflow-clip pt-0 transition-colors">
                 <img src={urlFor(album.mainImage).width(800).url() || ""} className="aspect-video object-cover" />
                 <CardHeader>
-                    <CardTitle>{album?.title}</CardTitle>
+                    <CardTitle>
+                        <div className="overflow-clip">
+                            <div className="flex -translate-x-[20px] items-center transition-all group-hover:translate-x-0">
+                                <ArrowRight size={"20"} />
+                                {album?.title}
+                            </div>
+                        </div>
+                    </CardTitle>
                     <CardDescription className="line-clamp-2">{album?.description}</CardDescription>
                 </CardHeader>
             </Card>
